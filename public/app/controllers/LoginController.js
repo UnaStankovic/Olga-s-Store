cb675b4540ebfdf77090fe757cd290e098749995
Una Stankovic <una_stankovic@yahoo.com>
2017-01-03 02:17:01 +0100

    Error on logging in added.

--- public/app/controllers/LoginController.js
+++ public/app/controllers/LoginController.js
@@ -1,11 +1,13 @@
-angular.module("Store").controller('LoginController', function($http){
-  this.info = {};
-  this.loginUser = function() {
-    console.log(this.info);
-    $http.post('../api/login',this.info)
+angular.module("Store").controller('LoginController', function($scope, $http){
+  $scope.info = {};
+  $scope.loginUser = function() {
+    console.log($scope.info);
+    $http.post('../api/login',$scope.info)
    .then(function(response) {
-       if(response.data.status == "error")
+       if(response.data.status == "error"){
+        $scope.info.errormsg = response.data.message;
         console.log("Error on logging in: " + response.data.message);
+      }
        else {
          console.log("Successfully logged in.");
        }
