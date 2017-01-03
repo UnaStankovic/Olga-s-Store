b5df6b78985b43dedaeb022467174fc339784b37
dicace <dimiccedomir@gmail.com>
2017-01-03 15:27:52 +0100

    Added services GET /api/product/{id}, PUT /api/product/{id}, DELETE /api/product/{id}

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -19,4 +19,61 @@ class ProductController extends Controller {
         $res->products = $products;
         return response()->json($res);
     }
+    
+      public function getProduct($id) {
+        
+        $res = new \stdClass();
+
+        if(!isAuthenticated() || !isAuthorized($_SESSION['userId'], 'A|^', $id))
+            return response()->json(errorResponse($res, 'Not authorized', 'permission'));
+
+        $product = DB::table('Product')->where('id', intval($id))->get();
+        if(count($product) == 0)
+            return response()->json(errorResponse($res, 'There is no such product', 'id'));
+
+        $res->status = 'success';
+        $res->product = $product[0];
+
+        return response()->json($res);
+    }
+    
+     public function updateProduct(Request $request, $id) {
+        $res = new \stdClass();
+
+        if(!isAuthenticated() || !isAuthorized($_SESSION['userId'], 'A|^', $id)) {
+            return response()->json(errorResponse($res, 'Not authorized', 'permission'));
+        }
+
+        $data = $request->all();
+        if(isset($data['id']) || isset($data['Category_id'])) {
+            return response()->json(errorResponse($res, 'There is one or more columns that can\'t be changed', 'illegal_update'));
+        }
+
+        $product = DB::table('Product')->where('id', intval($id));
+        if(count($product->get()) == 0) {
+            return response()->json(errorResponse($res, 'There is no such product', 'id'));
+        }
+        $product->update($data);
+
+        $res->status = 'success';
+        $product = $product->get();
+        $res->product = $product[0];
+        return response()->json($res);
+    }
+    
+    function deleteProduct($id) {
+        $res = new \stdClass();
+
+        if(!isAuthenticated() || !isAuthorized($_SESSION['userId'], 'A|^', $id))
+            return response()->json(errorResponse($res, 'Not authorized', 'permission'));
+
+        $product = DB::table('Product')->where('id', intval($id));
+        if(count($product->get()) == 0) {
+            return response()->json(errorResponse($res, 'There is no such product', 'id'));
+        }
+        $product->delete();
+
+        $res->status = 'success';
+        return response()->json($res);
+    }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,7 +3,7 @@
 /*
 |--------------------------------------------------------------------------
 | Application Routes
-|--------------------------------------------------------------------------
+|---------------------------------------    -----------------------------------
 |
 | Here is where you can register all of the routes for an application.
 | It is a breeze. Simply tell Lumen the URIs it should respond to
@@ -28,7 +28,10 @@ $app->put('api/user/{id}', 'UserController@changeUser');
 $app->delete('api/user/{id}', 'UserController@deleteUser');
 
 //product
-$app->get('api/Product', 'ProductController@index');
+$app->get('api/product', 'ProductController@index');
+$app->get('api/product/{id}', 'ProductController@getProduct');
+$app->put('api/product/{id}', 'ProductController@updateProduct');
+$app->delete('api/product/{id}', 'ProductController@deleteProduct');
 
 //has
 $app->get('api/has', 'HasController@index');
