30f81b59ed5f8c9d818b97ebae88adf60acd9a85
Una Stankovic <una_stankovic@yahoo.com>
2017-01-09 15:25:02 +0100

    Fixed some issues with forms and submits.

--- public/app/controllers/ChangeinfoController.js
+++ public/app/controllers/ChangeinfoController.js
@@ -1,14 +1,14 @@
 angular.module("Store").controller('ChangeinfoController', function($scope, $http, $rootScope){
-  $scope.info = {};
+  $scope.$parent.info = {};
   $http.get('../api/user/' + $rootScope.userid)
     .then(function(response) {
-      $scope.info = response.data.user;
+      $scope.$parent.info = response.data.user;
     });
-  $scope.changeInfo = function(){
-    $http.put('../api/user/' + $rootScope.userid, $scope.info)
+  $scope.$parent.changeInfo = function(){
+    $http.put('../api/user/' + $rootScope.userid, $scope.$parent.info)
     .then(function(response){
       if(response.data.status = 'success'){
-        $scope.info = response.data.user;
+        $scope.$parent.info = response.data.user;
         console.log(response.data);
       }
       else{
