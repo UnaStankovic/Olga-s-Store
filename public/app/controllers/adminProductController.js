c58cf769ff98ec680d233a88695b375e8ccf66eb
Una Stankovic <una_stankovic@yahoo.com>
2017-01-23 00:30:45 +0100

    Admin panel restructured.

--- public/app/controllers/adminProductController.js
+++ public/app/controllers/adminProductController.js
@@ -15,4 +15,20 @@ angular.module("Store").controller('aProductController', function($scope, $http,
         }
     });
   }
+
+  $scope.ChangeProduct = function(){
+    $http.post('../api/products', $scope.product)
+      .then(function(response) {
+        if(response.data.status == "error"){
+          $scope.product.errormsg_product = response.data.message;
+        //  console.log($scope.product.errormsg_product);
+        }
+        else {
+          $scope.product = response.data;
+          $scope.product.successmsg_product = "Successfully added";
+        //  console.log($scope.product);
+        }
+    });
+  }
+
 });
