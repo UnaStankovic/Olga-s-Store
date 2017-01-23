ec80487e50cdbfaf61daa59cfeb5dc832327fd2a
Una Stankovic <una_stankovic@yahoo.com>
2017-01-23 03:00:14 +0100

    Added search for products.

--- public/app/controllers/adminProductController.js
+++ public/app/controllers/adminProductController.js
@@ -16,16 +16,29 @@ angular.module("Store").controller('aProductController', function($scope, $http,
     });
   }
 
+  $scope.searchProduct = function(){
+    $http.get('../api/products/' + $scope.product.id)
+      .then(function(response) {
+        if(response.data.status == 'success'){
+          $scope.product = response.data.user;
+          console.log($scope.product);
+        }
+        else{
+          $scope.product.errormsg_searchproduct = response.data.message;
+            console.log($scope.product.errormsg_searchproduct);
+        }
+      });
+  };
   $scope.ChangeProduct = function(){
-    $http.post('../api/products', $scope.product)
+    $http.put('../api/products/', + $scope.product.id, $scope.info)
       .then(function(response) {
         if(response.data.status == "error"){
-          $scope.product.errormsg_product = response.data.message;
-        //  console.log($scope.product.errormsg_product);
+          $scope.product.errormsg_chproduct = response.data.message;
+          console.log($scope.product.errormsg_chproduct);
         }
         else {
           $scope.product = response.data;
-          $scope.product.successmsg_product = "Successfully added";
+          $scope.product.successmsg_chproduct = "Successfully changed";
         //  console.log($scope.product);
         }
     });
